2bc12481437610e3e831360a4021e1af34e6a8d9
pavel-sobchenko <pavel.sobchanka@gmail.com>
2024-01-31 02:10:06 +0100

    add zip/unzip files

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib'
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const targetFilePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
     // Write your code here 
+    try {
+        const input = fs.createReadStream(sourceFilePath);
+        const output = fs.createWriteStream(targetFilePath);
+        const gzip = zlib.createGzip();
+
+        input.pipe(gzip).pipe(output);
+
+        output.on('finish', () => {
+            console.log('File compressed successfully.');
+        });
+
+        output.on('error', (err) => {
+            console.error('Error compressing file:', err);
+        });
+    }
+    catch(error) {
+        console.error(error.message);
+    }
 };
 
 await compress();
